dfc852a3303a634a03bd13d02d2b51168ef2191f
manu-devops98 <manuelcappello7@gmail.com>
2021-12-21 15:18:27 +0100

    add click su contatti

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 /* Milestone 1
 - Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
 - Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
 Milestone 2
-Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
-Click sul contatto mostra la conversazione del contatto cliccato */
+- Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+- Click sul contatto mostra la conversazione del contatto cliccato */
 
 const app = new Vue({
     el: '#app',
@@ -94,9 +95,29 @@ const app = new Vue({
               ],
             },
           ],
-          counter: null
+          counter: 0
     },
     methods: {
-     
+      changeChat: function(index) {
+            this.counter = index;
+      },
+      isActive: function(index) {
+        if (this.counter == index) {
+          return 'active';
+        } else {
+          return '';
+        }
+      },
+      lastMessageDate: function(index, typeofmessage) {
+        const array = this.contacts[index].messages;
+        const arrayLength = array.length - 1;
+        if (typeofmessage == 'text') {
+            const lastText = array[arrayLength].text;
+            return lastText;
+        } else if (typeofmessage == 'date') {
+          const lastDate = array[arrayLength].date;
+          return lastDate;
+        }
+      }
     }
   });
